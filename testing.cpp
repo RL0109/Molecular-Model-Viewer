274dd308029bfb36183225da0454e611d1a37386
unknown <rwl54@scarletmail.rutgers.edu>
2026-07-06 18:17:49 -0400

    Adjusted testing logic to match the pdb_file_parser centerCoordinate logic.

--- testing.cpp
+++ testing.cpp
@@ -14,23 +14,31 @@ class ParsingTests : public Cppunit {
         void single_test() override {
             int failedAmount = 0;
             ifstream molecularFile("LEU.cif");
-            PDBFileParser parsedfile(molecularFile);
+            PDBFileParser parsedFile(molecularFile);
 
-            //Unit test can only be tested in long long, this centers the coordinates and multiplies by
-            //1000 to avoid truncation.
-            float expected_x = ((16.293 - parsedfile.sum.x));
-            float expected_y = ((15.907 - parsedfile.sum.y));
-            float expected_z = ((52.123 - parsedfile.sum.z));
+            float expected_x = ((16.293 - parsedFile.sum.x/parsedFile.atomData.size()));
+            float expected_y = ((15.907 - parsedFile.sum.y/parsedFile.atomData.size()));
+            float expected_z = ((52.123 - parsedFile.sum.z/parsedFile.atomData.size()));
 
 
             // Epsilon Testing
             float epsilon = 0.0011f;
 
-            CHECKT((parsedfile.atomData[0].position.x - expected_x) < epsilon);
-            
-            CHECKT((parsedfile.atomData[0].position.y - expected_y) < epsilon);
-            
-            CHECKT((parsedfile.atomData[0].position.z - expected_z) < epsilon);
+            CHECKT(std::abs(parsedFile.atomData[0].position.x - expected_x) < epsilon);
+            if (fails > failedAmount) {
+                failedAmount++;
+                std::cout << " Failed value: " << (parsedFile.atomData[0].position.x - expected_x) << "\n";
+            }
+            CHECKT(std::abs(parsedFile.atomData[0].position.y - expected_y) < epsilon);
+            if (fails > failedAmount) {
+                failedAmount++;
+                std::cout << " Failed value: " << (parsedFile.atomData[0].position.y - expected_y) << "\n";
+            }
+            CHECKT(std::abs(parsedFile.atomData[0].position.z - expected_z) < epsilon);
+            if (fails > failedAmount) {
+                failedAmount++;
+                std::cout << " Failed value: " << (parsedFile.atomData[0].position.z - expected_z) << "\n";
+            }
             status();
         }
 };
@@ -38,11 +46,7 @@ class ParsingTests : public Cppunit {
 int main() {
     ParsingTests testSuite;
     testSuite.single_test();
-    if (testSuite.fails > 0) {
-        std::cout << " " << testSuite.fails << " failed test(s)" << "\n";
-        return 0;
-    }
-    std::cout << "All tests passed successfully!" << "\n";
+    testSuite.status();
     return 0;
 
 }
